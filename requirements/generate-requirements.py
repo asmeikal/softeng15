c7c29af2debecaa932be1d148b24f4cddc9c43c8
Michele Laurenti <asmeikal@me.com>
2016-08-21 10:32:45 +0200

    Aggiorna requisiti sistema

--- requirements/generate-requirements.py
+++ requirements/generate-requirements.py
@@ -6,6 +6,10 @@ template = 'generated/templates/requirement.tex'
 sources  = 'generated/yaml/'
 output   = 'generated/system_requirements_table.tex'
 
+func_header = "\\subsection{Specifica dei Requisiti Funzionali}\n\n"
+n_func_header = "\\subsection{Specifica dei Requisiti Non Funzionali}\n\n"
+domain_header = "\\subsection{Specifica dei Requisiti di Dominio}"
+
 fields = [
     'title',
     'id',
@@ -31,12 +35,13 @@ class Requirement(object):
         self.index = index
         self.build_id()
         assert set(self.data.keys()) == set(fields)
+        self.type = self.data['type']
 
     def build_id(self):
         info = ['REQ']
         info += [self.data['title'].replace(' ','').upper()[0:6]]
-        info += [self.data['type']]
-        info += [self.data['priority']]
+        info += [self.data['type'].upper()[0]]
+        info += [self.data['priority'].upper()[0]]
         info += [str(index)]
         self.data['id'] = "\\code{{{}}}".format("\\_".join(info))
 
@@ -51,8 +56,23 @@ if __name__ == '__main__':
     files = [f for f in listdir(sources) if isfile(join(sources, f))]
     index = 0
     with open(output, 'w') as out:
+        res_func = ""
+        res_n_func = ""
+        res_domain = ""
         for f in files:
+            if f == "example.yaml": continue
             index += 1
             r = Requirement(sources + f, index)
-            out.write(r.output())
+            if r.type == 'Funzionale':
+                res_func += r.output()
+            elif r.type == 'Non Funzionale':
+                res_n_func += r.output()
+            elif r.type == 'Dominio':
+                res_domain += r.output()
+        out.write(func_header)
+        out.write(res_func)
+        out.write(n_func_header)
+        out.write(res_n_func)
+        out.write(domain_header)
+        out.write(res_domain)
 
